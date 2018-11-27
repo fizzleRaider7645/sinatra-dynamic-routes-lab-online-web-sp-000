f93420108dfdc2fb1c208ae34b8ecd3aa3c700f9
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-27 23:04:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     "#{@phrase}" * @number.to_i
   end
   
-  get /say/:word1/:word2/:word3/:word4/:word5 do
+  get '/say/:word1/:word2/:word3/:word4/:word5 do
     status 200
     @word1 = params[:word1]
     @word2 = params[:word2]
