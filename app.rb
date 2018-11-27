1791f1f0f83f86d8cd31e2898f5ad34554b22eb8
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-27 23:13:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -44,7 +44,7 @@ class App < Sinatra::Base
       when "multiply"
         "#{@number1 * @number2}"
       when "divide"
-        @number1 / @number2
+        "#{@number1 / @number2}""
     end
   end
 end
