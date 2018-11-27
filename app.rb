4c2c9800186b6429119614cbbbb106f054605aca
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-27 23:11:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -34,8 +34,8 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     status 200
     @operation = params[:operation]
-    @number1 = params[:number1]
-    @number2 = params[:number2]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
     case @oper
       when "subtract"
         (@number1 - @num2).to_s
